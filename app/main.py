693efc7986aa3b5e4fe82c8067fcec5b89e699be
juanda <jdrincone@gmail.com>
2025-10-09 21:53:36 -0500

    "feat: Add business logic for NaN values in payload"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import logging
 import pandas as pd
+import numpy as np
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
-
 from . import database
 from .schemas import HousingFeatures
 from src.data_manager import load_pipeline
@@ -32,19 +32,30 @@ def health_check():
 def predict(payload: HousingFeatures, db: Session = Depends(get_db)):
     """Realiza una predicción y la guarda en la base de datos."""
     logger.info(f"Received prediction request: {payload.model_dump()}")
+    payload_dict = payload.model_dump()
 
-    input_df = pd.DataFrame([payload.model_dump()])
-    input_df = input_df[FEATURES]
+    # TODO: La lógica de la condición de negocio
+    if np.isnan(payload_dict.get('RM')) and np.isnan(payload_dict.get('LSTAT')):
+        prediction_value = 0.0
+        logger.info("RM and LSTAT are NaN. Prediction is 0.")
+    else:
+        try:
+            input_df = pd.DataFrame([payload_dict])
+            input_df = input_df[FEATURES]
+            prediction_value = pipeline.predict(input_df)[0]
 
+        except Exception as e:
+            logger.error(f"Prediction error: {e}", exc_info=True)
+            db.rollback()
+            raise HTTPException(status_code=400, detail=f"Prediction error: {str(e)}")
+
+    # Continuamos con el resto de la lógica (guardado en la base de datos)
     try:
-        prediction_value = pipeline.predict(input_df)[0]
-        prediction_inputs = {key.lower(): value for key, value in payload.model_dump().items()}
-
+        prediction_inputs = {key.lower(): value for key, value in payload_dict.items()}
         db_prediction = database.Prediction(
             prediction_value=prediction_value,
-            **prediction_inputs  # Usamos el nuevo diccionario
+            **prediction_inputs
         )
-
         db.add(db_prediction)
         db.commit()
         db.refresh(db_prediction)
@@ -53,6 +64,6 @@ def predict(payload: HousingFeatures, db: Session = Depends(get_db)):
         return {"prediction": prediction_value}
 
     except Exception as e:
-        logger.error(f"Prediction error: {e}", exc_info=True)
+        logger.error(f"Database save error: {e}", exc_info=True)
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Prediction error: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Database save error: {str(e)}")
